0c82f959dc58f5912f9bb3a92000e38eceecbf91
Yashasvaddi <yashas.vaddi12@gmail.com>
2024-12-18 03:35:50 +0530

    Newer logic

--- streamlitclock.py
+++ streamlitclock.py
@@ -1,6 +1,15 @@
 import streamlit as st
 import time as t
 
-while(True):
-    timestamp=t.strftime("%H:%M:%S:%p")
-    st.write(timestamp)
+# Create a placeholder
+timestamp_placeholder = st.empty()
+
+while True:
+    # Get current time
+    timestamp = t.strftime("%H:%M:%S:%p")
+    
+    # Update the placeholder with the new timestamp
+    timestamp_placeholder.write(timestamp)
+    
+    # Pause for 1 second before updating again
+    t.sleep(1)
