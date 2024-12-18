5342224e4eba5d897c7009c17a4a3657a8ec0ccd
Yashasvaddi <yashas.vaddi12@gmail.com>
2024-12-18 18:01:30 +0530

    Wishing logic added

--- streamlitclock.py
+++ streamlitclock.py
@@ -10,6 +10,13 @@ while True:
     
     # Update the placeholder with the new timestamp
     timestamp_placeholder.write(timestamp)
-    
+    if int(t.strftime("%H"))>=0 and int(t.strftime("%H"))<12 and t.strftime("%p")=="AM":
+        st.write("Good Morning!!")
+    if int(t.strftime("%H"))>=12 and int(t.strftime("%H"))<5 and t.strftime("%p")=="PM":
+        st.write("Good Afternoon!!")
+    if int(t.strftime("%H"))>=5 and int(t.strftime("%H"))<8 and t.strftime("%p")=="PM":
+        st.write("Good Evening!!")
+    if int(t.strftime("%H"))>=8 and int(t.strftime("%H"))<12 and t.strftime("%p")=="PM":
+        st.write("Good Night!!")
     # Pause for 1 second before updating again
     t.sleep(1)
