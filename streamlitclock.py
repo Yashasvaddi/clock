c1a8cadf9c7ade9ead686667f1fa33ed86bca88f
Yashasvaddi <yashas.vaddi12@gmail.com>
2024-12-18 18:39:27 +0530

    new logo added

--- streamlitclock.py
+++ streamlitclock.py
@@ -4,11 +4,11 @@ import time as t
 # Create a placeholder
 timestamp_placeholder = st.empty()
 datestamp_holder=st.empty()
+st.image([removal.ai]_798fcf3a-6273-4e9e-8729-aec432569a21-dark-blue-modern-professional-technology-company-logo-2 (1) (1).png)
 
 while True:
     # Get current time
     timestamp = t.strftime("%H:%M:%S:%p")
-    
     # Update the placeholder with the new timestamp
     timestamp_placeholder.write(timestamp)
     if int(t.strftime("%H"))>=0 and int(t.strftime("%H"))<12 and t.strftime("%p")=="AM":
