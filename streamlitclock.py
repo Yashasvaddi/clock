bb201b71a60a99d4bb78544366da77c37b41b51d
Yashasvaddi <yashas.vaddi12@gmail.com>
2024-12-18 18:12:50 +0530

    new logic

--- streamlitclock.py
+++ streamlitclock.py
@@ -12,11 +12,11 @@ while True:
     timestamp_placeholder.write(timestamp)
     if int(t.strftime("%H"))>=0 and int(t.strftime("%H"))<12 and t.strftime("%p")=="AM":
         st.write("Good Morning!!")
-    if int(t.strftime("%H"))>=12 and int(t.strftime("%H"))<5 and t.strftime("%p")=="PM":
+    if int(t.strftime("%H"))>=12 and int(t.strftime("%H"))<17 and t.strftime("%p")=="PM":
         st.write("Good Afternoon!!")
-    if int(t.strftime("%H"))>=5 and int(t.strftime("%H"))<8 and t.strftime("%p")=="PM":
+    if int(t.strftime("%H"))>=17 and int(t.strftime("%H"))<20 and t.strftime("%p")=="PM":
         st.write("Good Evening!!")
-    if int(t.strftime("%H"))>=8 and int(t.strftime("%H"))<12 and t.strftime("%p")=="PM":
+    if int(t.strftime("%H"))>=20 and int(t.strftime("%H"))<24 and t.strftime("%p")=="PM":
         st.write("Good Night!!")
     # Pause for 1 second before updating again
     t.sleep(1)
