9083e50252ff9ae0d9a3630dae5aa4453e62879a
Yashasvaddi <yashas.vaddi12@gmail.com>
2024-12-18 19:11:03 +0530

    Converted from 24 hr format to 12 hrs format

--- streamlitclock.py
+++ streamlitclock.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import time as t
+from datetime import timedelta
 
 # Create a placeholder
 timestamp_placeholder = st.empty()
@@ -9,11 +10,12 @@ imagestamp_holder=st.empty()
 while True:
     # Get current time
     timestamp = t.strftime("%H:%M:%S:%p")
+    newtime=timestamp+timedelta(hours=5,minutes=30)
     hour=int(t.strftime("%H"))+5
     mins=int(t.strftime("%M"))+30
     timeofday=t.strftime("%p")
     # Update the placeholder with the new timestamp
-    timestamp_placeholder.write(int(t.strftime("%H"))+5:int(t.strftime("%M"))+30:int(t.strftime("%S")):int(t.strftime("%p"))
+    timestamp_placeholder.write(newtime)
     if hour>=0 and hour<12 and timeofday=="AM":
         datestamp_holder.write("Good Morning!!")
     if hour>=12 and hour<17 and timeofday=="PM":
